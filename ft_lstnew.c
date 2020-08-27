68df13a41266cfc8e0892caf95db56aee09abcff
YJ <myj0615@gmail.com>
2020-08-28 01:28:21 +0900

    200827 fix norm

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,15 +6,15 @@
 /*   By: yomoon <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/26 17:49:53 by yomoon            #+#    #+#             */
-/*   Updated: 2020/08/26 18:54:44 by yomoon           ###   ########.fr       */
+/*   Updated: 2020/08/28 01:27:14 by yomoon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list  *ft_lstnew(void *content)
+t_list		*ft_lstnew(void *content)
 {
-	t_list *new;
+	t_list	*new;
 
 	new = (t_list *)malloc(sizeof(t_list));
 	if (!new)
