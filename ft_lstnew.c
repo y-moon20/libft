83624d0ca86e3d8ce361f7964132b6e8120f82d0
YJ <myj0615@gmail.com>
2020-08-27 23:49:45 +0900

    200826 lst시작

---
+++ ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yomoon <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/26 17:49:53 by yomoon            #+#    #+#             */
+/*   Updated: 2020/08/26 18:54:44 by yomoon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list  *ft_lstnew(void *content)
+{
+	t_list *new;
+
+	new = (t_list *)malloc(sizeof(t_list));
+	if (!new)
+		return (0);
+	new->content = content;
+	new->next = NULL;
+	return (new);
+}
---
+++ ft_strtrim.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yomoon <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/23 15:27:42 by yomoon            #+#    #+#             */
+/*   Updated: 2020/08/25 16:32:18 by yomoon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*trim;
+	int		start;
+	int		end;
+	int		len;
+
+	if (!s1 || !set)
+		return (0);
+	start = 0;
+	while (s1[start] != '\0' && ft_strchr(set, s1[start]))
+		start++;
+	end = ft_strlen(s1) - 1;
+	while (end >= 0 && ft_strchr(set, s1[end]))
+		end--;
+	len = end - start + 1;
+	if (len <= 0)
+		len = 0;
+	trim = (char *)malloc(sizeof(char) * (len + 1));
+	if (!trim)
+		return (0);
+	ft_strlcpy(trim, s1 + start, len + 1);
+	return (trim);
+}	
