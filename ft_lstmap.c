c927a6a08185e84db68eb01dae08b5c44cb27a60
YJ <myj0615@gmail.com>
2020-08-30 02:51:11 +0900

    200830 첫완성

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -1,26 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yomoon <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/08/28 02:01:00 by yomoon            #+#    #+#             */
-/*   Updated: 2020/08/30 02:37:52 by yomoon           ###   ########.fr       */
+/*   Created: 2020/08/28 02:13:28 by yomoon            #+#    #+#             */
+/*   Updated: 2020/08/30 01:32:09 by yomoon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstclear(t_list **lst, void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*temp;
+	t_list	*new_list;
+	t_list	*new_element;
 
-	while (*lst && del)
+	if (!lst || !f)
+		return (0);
+	new_list = 0;
+	while (lst)
 	{
-		temp = (*lst)->next;
-		ft_lstdelone(*lst, del);
-		*lst = temp;
+		new_element = ft_lstnew(f(lst->content));
+		if (!new_element)
+		{
+			if (!del)
+				return (0);
+			ft_lstclear(&new_list, del);
+		}
+		ft_lstadd_back(&new_list, new_element);
+		lst = lst->next;
 	}
-	*lst = NULL;
+	return (new_list);
 }
